67baf577310819923ac2b97cc99276e2522bfb40
Jordan Littell <jordanlittell@gmail.com>
2023-10-04 09:56:21 -0700

    Add link to encryption algorithms for JWE

--- lib/encryption/jwe.ts
+++ lib/encryption/jwe.ts
@@ -2,4 +2,6 @@
  * JSON Web Encryption (JWE) represents encrypted content using JSON-based data structures. Note that signatures
  * are not supported in this standard
  * RFC: https://datatracker.ietf.org/doc/html/rfc7516
+ *
+ * Algorithms that should be supported are here: https://datatracker.ietf.org/doc/html/rfc7518#section-4.1
  */
