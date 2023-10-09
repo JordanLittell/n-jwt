f3162276063f3b0cbae46ac858c51bd2005c30c1
Jordan Littell <jordanlittell@gmail.com>
2023-10-09 11:31:27 -0700

    trying stuff

--- lib/signing/Signer.ts
+++ lib/signing/Signer.ts
@@ -3,6 +3,6 @@ import {JWK} from "@lib/jwk/jwk";
 /**
  * Uses a JWK to sign a payload
  */
-export interface Signer {
+export default interface Signer {
     sign(message: string, key: JWK) : string;
 }
